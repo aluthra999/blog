aca0e67968c33546e0ee89d58ad93eeadf693dbe
Ankit Luthra <aluthraxyz@gmail.com>
2023-11-07 01:11:23 -0400

    updates 11/6/23

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post
+from .models import Post, User
 from . import db
 
 views = Blueprint("views", __name__)
@@ -30,9 +30,40 @@ def create_post():
             db.session.add(post)
             db.session.commit()
             flash("Post created", category='success')
+            return redirect(url_for('views.home'))
     return render_template('create_post.html', user=current_user)
 
 
 @views.route("/contact")
 def contact():
     return render_template('contact.html', user=current_user)
+
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+
+    if not post:
+        flash("No such post found.", category="error")
+    elif current_user.id != post.author:
+        flash("You don't have permission to do that.", category="error")
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash("Post deleted successfully.", category='success')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash("User doesn't exist.", category='error')
+        return redirect(url_for('views.home'))
+
+    posts = Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
